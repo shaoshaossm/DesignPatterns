1ceeff42dee1d0e4e2162a68a68b0f4343d828d5
shaoshao <1600767556@qq.com>
2022-05-12 22:07:33 +0800

    泛华

---
+++ src/com/ssm/uml/generalization/DaoSupport.java
@@ -0,0 +1,14 @@
+package com.ssm.uml.generalization;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/5/12 22:04
+ */
+public abstract class DaoSupport {
+    public void save(Object entity) {
+    }
+
+    public void delete(Object id) {
+    }
+}
---
+++ src/com/ssm/uml/generalization/PersonServiceBean.java
@@ -0,0 +1,9 @@
+package com.ssm.uml.generalization;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/5/12 22:04
+ */
+public class PersonServiceBean extends DaoSupport {
+}

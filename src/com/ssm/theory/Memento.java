30bd828566edb37a151f72634b2655cc96526d13
shaoshao <1600767556@qq.com>
2022-05-30 21:11:37 +0800

    备忘录模式原理

---
+++ src/com/ssm/theory/Memento.java
@@ -0,0 +1,23 @@
+package com.ssm.theory;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/5/30 17:42
+ */
+public class Memento {
+    private String state;
+
+    //构造器
+    public Memento(String state) {
+        super();
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+
+
+}
